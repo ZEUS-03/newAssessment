45520404bd0739670abaddf12f40e55d9bda29e9
Gautam Sharma <64241254+ZEUS-03@users.noreply.github.com>
2025-01-06 10:37:54 +0530

    Update apiCallouts.jsx
    Added multiple api callouts

--- src/helper/apiCallouts.jsx
+++ src/helper/apiCallouts.jsx
@@ -4,8 +4,8 @@ export async function getUsers() {
   return await axios.get("https://testasdate.free.beeceptor.com/api/users");
 }
 
-export function validateUser(userData) {
-  const users = getUsers();
+export async function validateUser(userData) {
+  const { data: users } = await getUsers();
   const user = users.find((user) => user.email === userData.email);
   if (user) {
     if (user.password === userData.password) {
@@ -15,4 +15,10 @@ export function validateUser(userData) {
     }
   }
   return false;
+}
+
+export async function generateAvatar(fname, lname) {
+  const url = `https://ui-avatars.com/api/?name=${fname}+${lname}`;
+  const imageData = await axios.get(url);
+  return imageData;
 }
